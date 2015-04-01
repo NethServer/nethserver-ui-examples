bdb512de3e483766722f4f7be4d1f5c0f5ff9897
Stell0 <Stell0@users.noreply.github.com>
2015-04-01 18:00:22 +0200

    Update MyModule.php
    Template: php syntax error

--- selector-dropdown/Template/MyModule.php
+++ selector-dropdown/Template/MyModule.php
@@ -4,8 +4,7 @@ echo $view->header()->setAttribute('template', $T('MyModule_Title'));
 
 // add simple panel
 echo $view->panel()
-    ->insert($view->selector('MyValues', $view::SELECTOR_DROPDOWN))
-);
+    ->insert($view->selector('MyValues', $view::SELECTOR_DROPDOWN));
 
 // show submit and help buttons
 echo $view->buttonList($view::BUTTON_SUBMIT | $view::BUTTON_HELP);
